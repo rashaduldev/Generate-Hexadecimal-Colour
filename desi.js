edfe4ad49f9bbae54e6cc79292af00e43319e7f9
Rashadul Islam <rashadul.dev@gmail.com>
2023-05-17 22:54:31 +0600

    js complete

--- desi.js
+++ desi.js
@@ -3,10 +3,24 @@ window.onload=()=>{
     main();
 }
 function main(){
+    const main= document.getElementById('main');
+    const btn = document.getElementById('btn');
 
+    btn.addEventListener('click' , function(){
+        const bgcolour = generateRGB();
+        main.style.backgroundcolour=bgcolour;
+    });
 }
 
 // random rgb colour ganarator 
+function generateRGB(){
+  const red =  Math.floor(Math.random()*255);
+  const green =  Math.floor(Math.random()*255);
+  const blue =  Math.floor(Math.random()*255);
+  
+return`rgb(${red} , ${green} , ${blue})`;
+}
+
 
 
 // refarence connect
